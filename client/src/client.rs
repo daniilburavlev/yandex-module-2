c35b7a1273b43286f63a141bcfcd81ce24e71b3e
Daniil Buravlev <burravlev.d.a@gmail.com>
2025-12-08 22:10:25 +0300

    Client reads server response, validates OK, and adds subscription tests.

--- client/src/client.rs
+++ client/src/client.rs
@@ -1,7 +1,9 @@
 use log::info;
 use std::io;
-use std::io::Write;
+use std::io::{Read, Write};
 use std::net::{SocketAddr, TcpStream};
+
+const BUFFER_SIZE: usize = 1024;
 
 pub(crate) fn sub(addr: SocketAddr, remote: SocketAddr, tickers: Vec<String>) -> io::Result<()> {
     let mut stream = TcpStream::connect(remote)?;
@@ -9,5 +11,45 @@ pub(crate) fn sub(addr: SocketAddr, remote: SocketAddr, tickers: Vec<String>) ->
     info!("Sending request to {}: {}", remote, request);
     let _ = stream.write(request.as_bytes())?;
     stream.flush()?;
-    Ok(())
+    let mut buffer = [0u8; BUFFER_SIZE];
+    let size = stream.read(&mut buffer)?;
+    let buffer = &buffer[..size];
+    if size == 2 && buffer == b"OK" {
+        info!("Subscribed to: {}", tickers.join(","));
+        Ok(())
+    } else {
+        Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            String::from_utf8_lossy(buffer),
+        ))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Read;
+    use std::net::TcpListener;
+    use std::str::FromStr;
+    use std::thread;
+
+    #[test]
+    fn test_sub() {
+        let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+        thread::spawn(move || {
+            for stream in listener.incoming() {
+                let mut stream = stream.unwrap();
+                let mut buffer = [0u8; 1024];
+                let size = stream.read(&mut buffer).unwrap();
+                assert_eq!(&buffer[..size], b"SUB 127.0.0.1:9090 AAPL\r\n");
+                stream.write_all(b"OK\r\n").unwrap();
+            }
+        });
+        sub(
+            SocketAddr::from_str("127.0.0.1:9090").unwrap(),
+            SocketAddr::from_str("127.0.0.1:8080").unwrap(),
+            vec!["AAPL".to_string()],
+        )
+        .unwrap();
+    }
 }
